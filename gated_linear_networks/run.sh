09dea4afd616c6f6e64f7d4992c2397c68f2c99e
aisurfer <dbbvvpp@gmail.com>
2020-11-23 12:49:26 +0300

    replace several packages from requirements with pip install for ubuntu 16. Stuck with cuda version

--- gated_linear_networks/run.sh
+++ gated_linear_networks/run.sh
@@ -14,13 +14,21 @@
 # limitations under the License.
 set -e
 
-python3 -m venv gln_venv
+python3.6 -m venv gln_venv
 source gln_venv/bin/activate
-pip3 install --upgrade setuptools wheel
-pip3 install -r gated_linear_networks/requirements.txt
+python3.6 -m pip install --upgrade pip
+python3.6 -m pip install --upgrade setuptools wheel
+python3.6 -m pip install  --upgrade jax jaxlib
+python3.6 -m pip install  --upgrade tensorflow==2.3.1
+python3.6 -m pip install -r gated_linear_networks/requirements.txt
+python3.6 -m pip install --upgrade jax jaxlib==0.1.57+cuda102 -f https://storage.googleapis.com/jax-releases/jax_releases.html
 
 # Run MNIST example with Bernoulli GLN
-python3 -m gated_linear_networks.examples.bernoulli_mnist \
+#export XLA_PYTHON_CLIENT_MEM_FRACTION=.50
+#export XLA_PYTHON_CLIENT_ALLOCATOR=platform
+#XLA_PYTHON_CLIENT_PREALLOCATE=false
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda-10.2/lib64:/usr/local/cuda/extras/CUPTI/lib64:$LD_LIBRARY_PATH
+python3.6 -m gated_linear_networks.examples.bernoulli_mnist \
   --num_layers=2 \
   --neurons_per_layer=100 \
   --context_dim=1
